4926c03958acf550a88d0fcec45fb9a2ec0bad09
Ankur iPAHeartBeat <ranpariya.ankur@nukeboxstudios.com>
2023-06-29 13:33:40 +0530

    Boss Basic Behavior and attack system is added.

---
+++ Assets/Scripts/Boss/BossBehavior.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using iPAHeartBeat.Core.SignalSystem;
+
+using UnityEngine;
+
+public class BossBehavior : MonoBehaviour
+{
+	[SerializeField] private Animator animator;  // Animation System for Boss/character
+	[SerializeField] private int defaultDamage = 10;  // Default damage point
+	[SerializeField] private float attackInterval = 2f;  // Interval between attacks
+	[SerializeField] private bool autoAttack = false;  // Flag to enable auto-attack
+	[SerializeField] private bool attackDamageViaAnimation = false;  // Flag to Apply damage after or before animation finish
+	[SerializeField] private Attack[] attackStyles;  // Array of attack styles
+
+	private bool isActive = false;  // Flag to check if the boss is active
+	private bool canAttack = true;  // Flag to check if the boss can attack
+	private bool canApplyDamage = true;  // Flag to check if attack can damage or not. it's avoid double damage from Animation.
+	private Coroutine autoAttackCoroutine;  // Reference to the auto-attack coroutine
+	private Attack currentAttack = null;
+
+	// Function to activate the boss
+	public void ActivateBoss()
+	{
+		isActive = true;
+
+		if (autoAttack)
+			StartAutoAttack();
+	}
+
+	// Function to deactivate the boss
+	public void DeactivateBoss()
+	{
+		isActive = false;
+		StopAutoAttack();
+	}
+
+	// Function to perform an attack based on the given attack style index
+	public void Attack(int styleIndex)
+	{
+		if (!isActive || styleIndex < 0 || styleIndex >= attackStyles.Length || !canAttack)
+			return;
+
+		canAttack = false;
+		canApplyDamage = true;
+		currentAttack = attackStyles[styleIndex];
+		Debug.Log("Boss attacked with style " + styleIndex + " for " + currentAttack.damage + " damage.");
+		this.animator?.Play(currentAttack.attackStyle.ToString());
+		if (!attackDamageViaAnimation)
+		{
+			ApplyAttackDamage();
+		}
+
+		StartCoroutine(ResetAttack());
+	}
+
+	// Function will execute by the Animator when particular Attack animation finished.
+	public void ApplyAttackDamage()
+	{
+		if (canApplyDamage) return;
+
+		canApplyDamage = false;
+		var damageInfo = new BossAttackSignal
+		{
+			Damage = currentAttack?.damage ?? 0,
+		};
+
+		SignalManager.Me.Fire<BossAttackSignal>(damageInfo);
+	}
+
+	// Coroutine to reset the attack flag after the attack interval
+	private IEnumerator ResetAttack()
+	{
+		yield return new WaitForSeconds(attackInterval);
+		canAttack = true;
+	}
+
+	// Function to start auto-attack coroutine
+	private void StartAutoAttack()
+	{
+		autoAttackCoroutine ??= StartCoroutine(AutoAttack());
+	}
+
+	// Function to stop auto-attack coroutine
+	private void StopAutoAttack()
+	{
+		if (autoAttackCoroutine != null)
+		{
+			StopCoroutine(autoAttackCoroutine);
+			autoAttackCoroutine = null;
+		}
+	}
+
+	// Coroutine to perform auto-attack
+	private IEnumerator AutoAttack()
+	{
+		while (isActive && autoAttack)
+		{
+			if (canAttack)
+			{
+				int randomStyle = Random.Range(0, attackStyles.Length);
+				Attack(randomStyle);
+			}
+
+			yield return new WaitForSeconds(attackInterval);
+		}
+	}
+}
+
+// AttackStyle class to define an attack style with a specific damage point
+[System.Serializable]
+public class Attack
+{
+	public uint damage;  // Damage point for the attack style
+	public AttackStyles attackStyle;
+}
+
+public enum AttackStyles
+{
+	FireBullet,
+	FireMissile,
+	ThrowBomb,
+	Punch,
+	ClawSwipe,
+	BlowFire,
+	BlowAcid
+}
---
+++ Assets/Scripts/Signals/BossAttackSignal.cs
@@ -0,0 +1,3 @@
+public class BossAttackSignal {
+	public uint Damage;
+}
